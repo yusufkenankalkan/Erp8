9ff5b1446f37bdbf87078ccf6c458468547e1cb0
yusufkenankalkan <170403079@ogr.gelisim.edu.tr>
2023-03-01 14:26:46 +0300

    Form üstünde layout çalışmaları

--- RandevuSistemiForm/Form1.Designer.cs
+++ RandevuSistemiForm/Form1.Designer.cs
@@ -31,7 +31,18 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnUret = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.flp1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.flp1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -40,14 +51,17 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.flp1, 0, 3);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(610, 439);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(610, 473);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // textBox1
@@ -66,16 +80,113 @@
             this.textBox2.Size = new System.Drawing.Size(604, 23);
             this.textBox2.TabIndex = 1;
             // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.btnUret, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button2, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 61);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(604, 100);
+            this.tableLayoutPanel2.TabIndex = 2;
+            // 
+            // btnUret
+            // 
+            this.btnUret.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUret.Location = new System.Drawing.Point(3, 3);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(296, 43);
+            this.btnUret.TabIndex = 0;
+            this.btnUret.Text = "Mesai Saatleri Üret";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(305, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(296, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // flp1
+            // 
+            this.flp1.Controls.Add(this.button3);
+            this.flp1.Controls.Add(this.button4);
+            this.flp1.Controls.Add(this.button5);
+            this.flp1.Controls.Add(this.button7);
+            this.flp1.Controls.Add(this.button6);
+            this.flp1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flp1.Location = new System.Drawing.Point(3, 167);
+            this.flp1.Name = "flp1";
+            this.flp1.Size = new System.Drawing.Size(604, 303);
+            this.flp1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 45);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "8:45";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(122, 3);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 45);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(241, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(113, 45);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(360, 3);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(113, 45);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(479, 3);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(113, 45);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 439);
+            this.ClientSize = new System.Drawing.Size(610, 473);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.flp1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -85,5 +196,14 @@
         private TableLayoutPanel tableLayoutPanel1;
         private TextBox textBox1;
         private TextBox textBox2;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Button btnUret;
+        private Button button2;
+        private FlowLayoutPanel flp1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button7;
+        private Button button6;
     }
 }
--- RandevuSistemiForm/Form1.cs
+++ RandevuSistemiForm/Form1.cs
@@ -6,5 +6,46 @@ namespace RandevuSistemiForm
         {
             InitializeComponent();
         }
+
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            flp1.Controls.Clear();
+            for (int saat = 9; saat < 17; saat++)
+            {
+                if (saat == 12)
+                    continue;
+                for (int dakika = 0; dakika < 46; dakika += 15)
+                {
+                    int sonrakiSaat = saat;
+                    int sonrakiDakika = dakika + 15;
+                    if (sonrakiDakika == 60)
+                    {
+                        sonrakiSaat++;
+                        sonrakiDakika = 0;
+                    }
+                    string btnText = $"{saat:00}:{dakika:00} - {sonrakiSaat:00}:{sonrakiDakika:00}";
+                    Button yeniButon = ButonUret(btnText);
+                    flp1.Controls.Add(yeniButon);
+                }
+
+
+            }
+        }
+        private Button ButonUret(string btnText)
+        {
+            Button btn = new Button()
+            {
+                Text = btnText,
+                Size = new Size(66, 60),
+            };
+            btn.Click += BtnMesaiSaat_Click;
+            return btn;
+        }
+
+        private void BtnMesaiSaat_Click(object? sender, EventArgs e)
+        {
+            Button basilanButon = (Button)sender;
+            this.Text = basilanButon.Text;
+        }
     }
 }
