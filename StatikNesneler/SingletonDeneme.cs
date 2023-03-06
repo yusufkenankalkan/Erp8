138571b744465123000b253a8ca9198990236e40
yusufkenankalkan <170403079@ogr.gelisim.edu.tr>
2023-03-06 09:55:57 +0300

    Statik nesneler ve singleton

---
+++ StatikNesneler/SingletonDeneme.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatikNesneler
+{
+    public class SingletonDeneme
+    {
+        public Guid Id { get; set; }
+
+        private SingletonDeneme()
+        {
+            Thread.Sleep(10 * 1000);
+            this.Id = Guid.NewGuid();
+            
+        }
+        private static SingletonDeneme _singletonDeneme;
+        public static SingletonDeneme GetInstance()
+        {
+            //if (_singletonDeneme == null)
+            //    _singletonDeneme = new SingletonDeneme();
+            //return _singletonDeneme;
+
+            //Alttaki yazım üstteki ile aynı işlemi yapar
+
+            return _singletonDeneme ??= new SingletonDeneme(); // ?? operatörü null kontrolünü if komutu yazmadan yapar. Aşağıdaki örnekle aynı mantıkla çalışır.
+        }
+        public static SingletonDeneme Instance
+        {
+            get
+            {
+                if (_singletonDeneme == null)
+                    _singletonDeneme = new SingletonDeneme();
+                return _singletonDeneme;
+            }
+        }
+
+    }
+}
